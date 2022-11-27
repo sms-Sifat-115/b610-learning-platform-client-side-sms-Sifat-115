c3a56a654cb22bd118015b8b26e69d944aea61c9
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2022-11-27 08:46:58 +0600

    Second Draft: Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./Components/Main/Main";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      // children: [
+      //   {
+      //     path: "/",
+      //     element: <Home></Home>,
+      //   },
+      //   {
+      //     path: "/quiz",
+      //     element: <Quiz></Quiz>,
+      //   },
+      //   {
+      //     path: "/quiz/:id",
+      //     loader: async ({ params }) => {
+      //       return fetch(
+      //         `https://openapi.programming-hero.com/api/quiz/${params.id}`
+      //       );
+      //     },
+      //     element: <Questions></Questions>,
+      //   },
+      //   {
+      //     path: "/stats",
+      //     element: <Stats></Stats>,
+      //   },
+      //   {
+      //     path: "/blogs",
+      //     element: <Blogs></Blogs>,
+      //   },
+      //   {
+      //     path: "*",
+      //     element: <Error></Error>,
+      //   },
+      // ],
+    },
+  ]);
   return (
-    <div className="App">
-      
+    <div className>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
