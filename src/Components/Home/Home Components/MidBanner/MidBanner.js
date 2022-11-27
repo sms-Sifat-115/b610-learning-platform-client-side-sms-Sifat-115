35ad5aa40d46e7d6b995695e878b493bf6d0c60a
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2022-11-27 20:40:10 +0600

    Fourth Draft: Home > MidBanner Added

--- src/Components/Home/Home Components/MidBanner/MidBanner.js
+++ src/Components/Home/Home Components/MidBanner/MidBanner.js
@@ -1,1 +1,23 @@
 
+import React from "react";
+
+const MidBanner = () => {
+  return (
+    <div className="text-center my-10 mb-60">
+      <div>
+        <h1 className="text-3xl my-5">Skills you will Learn from Us</h1>
+        <div className="vertical lg:horizontal justify-evenly">
+          <button className="disabled mx-5 my-3 btn text-white btn-error">HTML 5</button>
+          <button className="disabled mx-5 my-3 btn btn-secondary">CSS 3</button>
+          <button className="disabled mx-5 my-3 btn btn-accent">JavaScript</button>
+          <button className="disabled mx-5 my-3 btn text-white btn-info">Reactjs</button>
+          <button className="disabled mx-5 my-3 btn text-white btn-warning">Firebase</button>
+          <button className="disabled mx-5 my-3 btn text-white btn-success">English</button>
+          <button className="disabled mx-5 my-3 btn text-white btn-error">Web Development</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MidBanner;
